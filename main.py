9b0b43aeddd01df683d6814daccc5725816837de
Ronald / <belloconcepcionr@gmail.com>
2025-12-28 10:30:10 -0500

    Add main.py and index.html from upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Form
+import requests
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import json
+
+app = FastAPI()
+
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+
+@app.get("/")
+def root():
+    return FileResponse("templates/index.html")
+
+@app.post("/request")
+async def req(
+    u: str = Form(...),
+    m: str = Form(...),
+    p: str = Form(...)
+):
+    response = ""
+    parms = json.loads(p) if p else None
+    if m.upper() == "GET":
+        response = requests.get(u, params=parms)
+    elif m.upper() == "POST":
+        response = requests.post(u, params=parms)
+    elif m.upper() == "PUT":
+        response = requests.put(u, params=parms)
+    elif m.upper() == "DELETE":
+        response = requests.delete(u, params=parms)
+    data = response.json()
+    return f'{data}'
+
