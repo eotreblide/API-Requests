e7c74500474a96b4690eee2a4bb29b2dbe062759
Ronald / <belloconcepcionr@gmail.com>
2025-12-28 10:46:53 -0500

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,23 @@
+const sendBtn = document.getElementById("sendBtn")
+const url = document.getElementById("url")
+const meth = document.getElementById("method")
+const txt = document.getElementById("result")
+const params = document.getElementById("jsonParams")
+async function send() {
+    try{
+        const form = new FormData()
+        form.append("u", url.value)
+        form.append("m", meth.value)
+        form.append("p", params.value)
+        let res = await fetch("/request", {
+            method: "POST",
+            body: form
+        })
+        const data = await res.json()
+        txt.textContent = data
+    } catch {
+        txt.textContent = `Ha ocurrido un error, porfavor revise si el metodo ${meth.value} es el correcto`
+    }
+}
+
+sendBtn.addEventListener('click', send)
